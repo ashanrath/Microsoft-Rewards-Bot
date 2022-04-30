d0981e6943cf17731714de67469cd80cf72d108c
Anthony Jiang <anthony.zyjiang@outlook.com>
2022-04-30 19:10:33 +0100

    updated UA_PC

--- env.py
+++ env.py
@@ -9,7 +9,5 @@ URL_BING_SEARCH = 'https://www.bing.com/search'
 URL_GTREND = 'https://trends.google.com/trends/api/dailytrends?hl=en-US&geo=US&ns=15'
 
 # User Agent
-USER_AGENT_PC = ('Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
-                 'AppleWebKit/537.36 (KHTML, like Gecko) '
-                 'Chrome/64.0.3282.140 Safari/537.36 Edge/17.17134')
+USER_AGENT_PC = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.74 Safari/537.36 Edg/99.0.1150.46'
 USER_AGENT_MOBILE = 'Mozilla/5.0 (iPhone; CPU iPhone OS 15_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/99.0.4844.59 Mobile/15E148 Safari/604.1'
