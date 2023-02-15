9098c71981712673af29072f368ec28fff6bfc1d
Jose Valecillos <89293+jvalecillos@users.noreply.github.com>
2023-02-15 11:41:10 +0100

    Fixing bug when copying chromedriver from downloaded file

--- helper/driver.py
+++ helper/driver.py
@@ -2,7 +2,6 @@ import os
 import zipfile
 import requests
 import env
-import shutil
 
 def update_driver():
     os.remove("drivers/chromedriver.exe")
@@ -20,20 +19,35 @@ def download_driver(driver_path, system):
     response = requests.get(url, stream=True)
     zip_file_path = os.path.join(os.path.dirname(
         driver_path), os.path.basename(url))
+    # save response into zipfile
     with open(zip_file_path, "wb") as handle:
         for chunk in response.iter_content(chunk_size=512):
             if chunk:  # filter out keep alive chunks
                 handle.write(chunk)
+    
     extracted_dir = os.path.splitext(zip_file_path)[0]
+    # extract zipfile
     with zipfile.ZipFile(zip_file_path, "r") as zip_file:
         zip_file.extractall(extracted_dir)
+    # remove zip
     os.remove(zip_file_path)
 
-    driver = os.listdir(extracted_dir)[0]
-    os.rename(os.path.join(extracted_dir, driver), driver_path)
-    shutil.rmtree(extracted_dir)
+    # get driver from extracted files
+    driver = None
+    for filename in os.listdir(extracted_dir):
+        if filename.lower().startswith("chromedriver"):
+            driver = filename
 
-    os.chmod(driver_path, 0o755)
-    # way to note which chromedriver version is installed
-    open(os.path.join(os.path.dirname(driver_path),
-                      "{}.txt".format(latest_version)), "w").close()
+    if driver:
+        # move driver to driver_path (parent folder)
+        os.rename(os.path.join(extracted_dir, driver), driver_path)
+        os.rmdir(extracted_dir)
+        # add execution permissions
+        os.chmod(driver_path, 0o755)
+        # way to note which chromedriver version is installed
+        open(os.path.join(os.path.dirname(driver_path),
+                        "{}.txt".format(latest_version)), "w").close()
+    else:
+        raise Exception("Error finding driver from downloaded file")
+
+
